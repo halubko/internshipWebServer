28114ef5344af3ac94a544380578705efeff0fe1
halubko <misha.golubko@gmail.com>
2026-01-27 01:31:00 +0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { PORT } from "@/constants/constants";
 import ProductsRouter from "./routers/ProductsRouter";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/products", ProductsRouter);
