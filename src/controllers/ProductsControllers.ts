0eedbf0de29e25e9690f7e915774e0bfec03e292
halubko <misha.golubko@gmail.com>
2026-01-27 20:28:46 +0300

    changed error handling with next function

--- src/controllers/ProductsControllers.ts
+++ src/controllers/ProductsControllers.ts
@@ -1,8 +1,9 @@
 import { CATEGORIES } from "@/constants/categories";
 import { CATEGORY_LIST } from "@/constants/categoryList";
 import ProductsServices from "@/services/ProductsServices";
+import ApiError from "@/utils/ApiError";
 import { createProductValidation } from "@/validation/createProductValidation";
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 
 class ProductsControllers {
    static async getAllProducts(req: Request, res: Response) {
@@ -16,23 +17,23 @@ class ProductsControllers {
       return res.status(200).json(products);
    }
 
-   static async getProductById(req: Request, res: Response) {
+   static async getProductById(req: Request, res: Response, next: NextFunction) {
       const { id } = req.params;
       const numberId = Number(id);
 
       if (!id || isNaN(numberId)) {
-         return res.status(400).json({ message: "Product ID is required" });
+         return next(new ApiError(400, "Product ID is required"));
       }
 
       const product = await ProductsServices.getProductById(numberId);
       return res.status(200).json(product);
    }
 
-   static async searchProductsByTitle(req: Request, res: Response) {
+   static async searchProductsByTitle(req: Request, res: Response, next: NextFunction) {
       const { skip, limit, q, order, sortBy } = req.query;
 
       if (!q) {
-         return res.status(400).json({ message: "Title query parameter is required" });
+         return next(new ApiError(400, "Title query parameter is required"));
       }
 
       const products = await ProductsServices.searchProductsByTitle(
@@ -45,12 +46,12 @@ class ProductsControllers {
       return res.status(200).json(products);
    }
 
-   static async getProductsByCategory(req: Request, res: Response) {
+   static async getProductsByCategory(req: Request, res: Response, next: NextFunction) {
       const { skip, limit, order, sortBy } = req.query;
       const { category } = req.params;
 
       if (!category) {
-         return res.status(400).json({ message: "Category query parameter is required" });
+         return next(new ApiError(400, "Category query parameter is required"));
       }
 
       const products = await ProductsServices.getProductsByCategory(
@@ -71,13 +72,11 @@ class ProductsControllers {
       return res.status(200).json(CATEGORY_LIST);
    }
 
-   static async createProduct(req: Request, res: Response) {
+   static async createProduct(req: Request, res: Response, next: NextFunction) {
       const validation = createProductValidation.safeParse(req.body);
 
       if (!validation.success) {
-         return res
-            .status(400)
-            .json({ message: "Validation failed", errors: validation.error.flatten().fieldErrors });
+         return next(new ApiError(400, "Validation error", validation.error.flatten().fieldErrors));
       }
 
       const newProduct = await ProductsServices.createProduct(req.body);
@@ -85,12 +84,12 @@ class ProductsControllers {
       return res.status(201).json(newProduct);
    }
 
-   static async deleteProduct(req: Request, res: Response) {
+   static async deleteProduct(req: Request, res: Response, next: NextFunction) {
       const { id } = req.params;
       const numberId = Number(id);
 
       if (!id || isNaN(numberId)) {
-         return res.status(400).json({ message: "Product ID is required" });
+         return next(new ApiError(400, "Product ID is required"));
       }
 
       const deletedProduct = await ProductsServices.deleteProduct(numberId);
