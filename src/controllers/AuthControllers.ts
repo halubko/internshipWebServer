23a12acd6e0385dd3d38982c863a27166ef82d19
halubko <misha.golubko@gmail.com>
2026-01-29 22:45:04 +0300

    refresh token controller

--- src/controllers/AuthControllers.ts
+++ src/controllers/AuthControllers.ts
@@ -1,6 +1,12 @@
+import type { User } from "@/prismaGenerated/client";
 import AuthServices from "@/services/AuthServices";
 import ApiError from "@/utils/ApiError";
-import { decodeAccessToken, generateAccessToken } from "@/utils/jwt";
+import {
+   decodeAccessToken,
+   decodeRefreshToken,
+   generateAccessToken,
+   generateRefreshToken,
+} from "@/utils/jwt";
 import loginValidation from "@/validation/loginValidation";
 import bcryprt from "bcrypt";
 import type { NextFunction, Request, Response } from "express";
@@ -33,7 +39,7 @@ class AuthController {
       const { password: _, ...userData } = user;
 
       const accessToken = generateAccessToken(userData, expiresInMins && `${expiresInMins}m`);
-      const refreshToken = generateAccessToken(userData, "7d");
+      const refreshToken = generateRefreshToken(userData);
 
       res.cookie("accessToken", accessToken, {
          maxAge: (expiresInMins ? expiresInMins : 60) * 1000,
@@ -68,6 +74,43 @@ class AuthController {
       const decoded = decodeAccessToken(token);
 
       return res.status(200).json(decoded);
+   }
+
+   static async refresh(req: Request, res: Response, next: NextFunction) {
+      const { refreshToken, expiresInMins } = req.body;
+
+      if (!refreshToken) {
+         return next(new ApiError(400, "Refresh token is missing"));
+      }
+
+      const decoded = decodeRefreshToken(refreshToken);
+
+      if (typeof decoded === "string") {
+         return next(new ApiError(403, "Invalid refresh token"));
+      }
+
+      const { exp: __, iat: _, ...userData } = decoded;
+
+      const accessToken = generateAccessToken(
+         userData as Omit<User, "password">,
+         expiresInMins && `${expiresInMins}m`
+      );
+
+      res.cookie("accessToken", accessToken, {
+         maxAge: (expiresInMins ? expiresInMins : 60) * 1000,
+         httpOnly: true,
+         secure: false,
+         sameSite: "strict",
+      });
+
+      res.cookie("refreshToken", refreshToken, {
+         maxAge: 7 * 24 * 60 * 60 * 1000,
+         httpOnly: true,
+         secure: false,
+         sameSite: "strict",
+      });
+
+      return res.status(200).json({ accessToken, refreshToken });
    }
 }
 
