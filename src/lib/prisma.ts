5cc1f627279c5ce020131088f24a4ff6dc0cb166
halubko <misha.golubko@gmail.com>
2026-01-29 21:09:57 +0300

    add password hash extend

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,27 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@/prismaGenerated/client";
+import { PrismaClient, type User } from "@/prismaGenerated/client";
+import bcrypt from "bcrypt";
+import { SALT } from "@/constants/constants";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({ adapter }).$extends({
+   query: {
+      user: {
+         async $allOperations({ operation, args, query }) {
+            if (["create", "update", "upsert"].includes(operation) && "data" in args) {
+               const data = args.data as User;
+
+               if (data && typeof data.password === "string") {
+                  data.password = bcrypt.hashSync(data.password, SALT);
+               }
+            }
+            return query(args);
+         },
+      },
+   },
+});
 
 export { prisma };
